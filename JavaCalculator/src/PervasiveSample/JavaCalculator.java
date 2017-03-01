e400813001487cf7622e8a78b667131665d1e8da
Fabrizio Nagy <fabrizio.nagy@fastwebnet.it>
2017-03-01 11:13:56 +0100

    First version of calculator

--- JavaCalculator/src/PervasiveSample/JavaCalculator.java
+++ JavaCalculator/src/PervasiveSample/JavaCalculator.java
@@ -1,9 +1,45 @@
 package PervasiveSample;
 
+import java.util.Scanner;
+
 public class JavaCalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double n1, n2;
+		String operation;
+		Scanner scanObject = new Scanner(System.in);
+
+		System.out.println("Enter first number");
+		n1 = scanObject.nextDouble();
+		System.out.println("Enter second number");
+		n2 = scanObject.nextDouble();
+
+		Scanner op = new Scanner(System.in);
+
+		System.out.println("Enter your operation");
+		operation = op.next();
+
+		switch (operation) {
+		case "+":
+			System.out.println("Your answer: " + (n1 + n2));
+			break;
+
+		case "-":
+			System.out.println("Your answer: " + (n1 - n2));
+			break;
+
+		case "*":
+			System.out.println("Your answer: " + (n1 * n2));
+			break;
+
+		case "/":
+			System.out.println("Your answer: " + (n1 / n2));
+			break;
+
+		default:
+			System.out.println("Unknown operation");
+		}
 
 	}
 
